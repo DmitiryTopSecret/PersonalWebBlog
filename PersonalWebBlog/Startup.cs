471f501831c8c10400ee9469368ce8645fba8d5f
dizimka2255@mail.ru <dizimka2255@mail.ru>
2020-09-01 17:52:33 +0300

    adding services for Startup.cs

--- PersonalWebBlog/Startup.cs
+++ PersonalWebBlog/Startup.cs
@@ -5,10 +5,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PersonalWebBlog.Domain;
+using PersonalWebBlog.Domain.Repositories.Abstract;
+using PersonalWebBlog.Domain.Repositories.EntityFramework;
 using PersonalWebBlog.Service;
 
 namespace PersonalWebBlog
@@ -22,8 +27,39 @@ namespace PersonalWebBlog
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // adding config from appsettings.json
             Configuration.Bind("Project", new Config());
 
+            //adding functionaltyt if app as services
+            services.AddTransient<ITextFieldsRepository, EFTextFieldsRepository>();
+            services.AddTransient<IServiceItemsRepository, EFServiceItemsRepository>();
+            services.AddTransient<DataManager>();
+
+            // adding DB context
+            services.AddDbContext<AppDBContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+            // identity system
+            services.AddIdentity<IdentityUser, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppDBContext>().AddDefaultTokenProviders();
+
+            //authentication cookie
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "myCompanyAuth";
+                options.Cookie.HttpOnly = true;
+                options.LoginPath = "/account/login";
+                options.AccessDeniedPath = "/account/accessdenied";
+                options.SlidingExpiration = true;
+            });
+
+            //services for controllers and views
             services.AddControllersWithViews()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
@@ -35,11 +71,13 @@ namespace PersonalWebBlog
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
             app.UseStaticFiles();
 
+            app.UseRouting();
 
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
