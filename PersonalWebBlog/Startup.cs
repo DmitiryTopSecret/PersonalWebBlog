d09684f05db6784bcba137dc0496b0c9822dbf1e
dizimka2255@mail.ru <dizimka2255@mail.ru>
2020-09-03 00:07:40 +0300

    added admin controller, view and login page connected witg database

--- PersonalWebBlog/Startup.cs
+++ PersonalWebBlog/Startup.cs
@@ -59,8 +59,16 @@ namespace PersonalWebBlog
                 options.SlidingExpiration = true;
             });
 
+            services.AddAuthorization(x =>
+            {
+                x.AddPolicy("AdminArea", policy => { policy.RequireRole("admin"); });
+            });
+
             //services for controllers and views
-            services.AddControllersWithViews()
+            services.AddControllersWithViews(x => 
+            {
+                x.Conventions.Add(new AdminAreaAuthorization("Admin", "AdminArea"));
+            })
                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
@@ -81,6 +89,7 @@ namespace PersonalWebBlog
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute("admin", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
